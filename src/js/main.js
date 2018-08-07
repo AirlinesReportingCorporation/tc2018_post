14a7ebdf741877fca2c88c31edaaf41734775b24
Ian Fajardo <ianfajardozx@gmail.com>
2018-08-07 16:57:25 -0400

    Stats and Highlights Sections

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,38 @@ import '../css/main.scss';
 import $ from 'jquery';
 import Vue from 'vue';
 import owlCarousel from 'owl.carousel';
+import TypeIt from 'typeit';
+
+function statsInit(){
+  var stats = $('.stats').length;
+  var delay = 200;
+
+  for(var i = 1; i <= stats; i++){
+    var e = '.stats' + i;
+    var f = '.stats-title' + i;
+    var g = '.stats-subtitle' + i;
+    var h = '.stats-image' + i;
+
+    $(h).addClass("fadeInDown");
+
+    new TypeIt(e, {
+      speed: 200,
+      cursor: false,
+      startDelay: i * delay
+    })
+
+    new TypeIt(f, {
+      speed: 100,
+      cursor: false,
+      startDelay: i * delay
+    })
+
+    new TypeIt(g, {
+      speed: 50,
+      cursor: false,
+      startDelay: i * delay
+    })
+  }
+}
+
+statsInit();
