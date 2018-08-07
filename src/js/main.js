54b257273768f01f49c451cb25eff5e8fac7c928
Ian Fajardo <ianfajardozx@gmail.com>
2018-08-07 13:30:15 -0400

    init commit

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+//css imports
+import '../css/main.scss';
+
+//js dependency imports
+import $ from 'jquery';
+import Vue from 'vue';
+import owlCarousel from 'owl.carousel';
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+ const path = require('path');
+ const ExtractTextPlugin = require("extract-text-webpack-plugin");
+ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+ const webpack = require('webpack');
+
+ const extractSass = new ExtractTextPlugin({
+    filename: "[name].min.css",
+    disable: process.env.NODE_ENV === "development"
+});
+
+  module.exports = [
+		{
+			entry: {
+					main: './src/js/main.js'
+			},
+			output: {
+				filename: '[name].min.js',
+				path: path.resolve(__dirname, 'dest')
+			},
+			module: {
+				rules: [
+					{
+						test: /\.js$/,
+						exclude: /(node_modules|bower_components)/,
+						use: {
+							loader: 'babel-loader',
+							options: {
+								presets: ['env'],
+								minified: true,
+								compact: true
+							}
+						}
+					},
+					{
+						test: /\.scss$/,
+						use: extractSass.extract({
+								use: [{
+										loader: "css-loader"
+								}, {
+										loader: "sass-loader",
+										options: {
+											outputStyle: "compressed"
+										}
+								}],
+								fallback: "style-loader"
+						})
+					}
+				]
+			},
+			resolve: {
+				alias: {
+					'masonry': 'masonry-layout',
+					'isotope': 'isotope-layout',
+          'vue': 'vue/dist/vue.js'
+				}
+			},
+			plugins: [
+        new webpack.ProvidePlugin({
+          $: 'jquery',
+          jquery: 'jquery',
+          'window.jQuery': 'jquery',
+          jQuery: 'jquery'
+        }),
+        extractSass,
+				new UglifyJsPlugin({
+          test: /\main.min.js($|\?)/i
+        })
+			]
+		}
+  ];
