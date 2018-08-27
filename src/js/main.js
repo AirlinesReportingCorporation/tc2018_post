773b72b4cb9ccaa7767d6a03329feb1b89a57cfc
ianfajardo <ianfajardozx@gmail.com>
2018-08-27 16:03:39 -0400

    Add leaflet maps and map block

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,34 @@ import $ from 'jquery';
 import Vue from 'vue';
 import owlCarousel from 'owl.carousel';
 import TypeIt from 'typeit';
+import L from 'leaflet';
+
+function mapInit() {
+  var mymap = L.map('mapid', {scrollWheelZoom: false}).setView([38.980, -77.017], 10);
+
+	var marker = L.marker([38.780, -77.017], {riseOnHover: true}).addTo(mymap);
+
+        var dulles = L.marker([38.953, -77.456], {riseOnHover: true}).addTo(mymap);
+
+        var bwi = L.marker([39.177, -76.668], {riseOnHover: true}).addTo(mymap);
+
+        var rr = L.marker([38.851294, -77.040282], {riseOnHover: true}).addTo(mymap);
+
+	marker.bindTooltip("<strong>Gaylord National Resort & Convention Center</strong><br/>TravelConnect 2018<br/>", {permanent: true }).openTooltip();
+
+dulles.bindTooltip("<strong>Washington Dulles International Airport</strong><br/>IAD<br/>", {permanent: true }).openTooltip();
+
+bwi.bindTooltip("<strong>Balitmore-Washington International Airport</strong><br/>BWI<br/>", {permanent: true }).openTooltip();
+
+rr.bindTooltip("<strong>Ronald Reagan Washington National Airport</strong><br/>DCA<br/>", {permanent: true }).openTooltip();
+
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+			maxZoom: 15,
+			id: 'mapbox.streets',
+			scrollWheelZoom: false,
+			accessToken: 'pk.eyJ1IjoiaWZhamFyZG8iLCJhIjoiY2o4MGJ5dHl3MjBpbzMzczBwd3EyeWV6ZiJ9.9LHEjhURIT4WFRpfc-rQRA'
+	}).addTo(mymap);
+}
 
 function statsInit(){
   var stats = $('.stats').length;
@@ -67,3 +95,4 @@ function statsInit(){
 
 
 statsInit();
+mapInit();
